a62423a804f50f1bbe77086ad96b752aadcfaca0
Tito Tawfik <tawfik.bakr@gmail.com>
2025-04-20 21:21:59 +0000

    WPForms integration and styling fixes

--- .devcontainer/wp-content/themes/theclinicapp/partials/form-section.php
+++ .devcontainer/wp-content/themes/theclinicapp/partials/form-section.php
@@ -2,7 +2,7 @@
 
 ?>
 
-<!-- Form Section -->
+<!-- Form Section Signup-->
 <section class="form-section text-center fade-on-scroll" id="signup">
     <div class="container">
         <div class="row justify-content-center">
@@ -19,31 +19,10 @@
             <div class="col-12">
                 <div class="form-card py-5">
                     <h4 class="fw-bold fs-4 mb-5">Get Started with TheClinicApp</h4>
-                    <form>
-                        <div class="row mb-3">
-                            <div class="col-md-6 mb-3 mb-md-0">
-                                <label for="fullName" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" id="fullName" placeholder="Dr. Jane Smith">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="clinicName" class="form-label">Clinic Name</label>
-                                <input type="text" class="form-control" id="clinicName" placeholder="Smith Family Practice">
-                            </div>
-                        </div>
-
-                        <div class="mb-4">
-                            <label for="email" class="form-label">Email Address</label>
-                            <input type="email" class="form-control" id="email" placeholder="jane.smith@example.com">
-                        </div>
-
-                        <div class="d-grid mb-3">
-                            <button type="submit" class="btn btn-primary trial-btn fs-6">Start Your Free 14-Day Trial</button>
-                        </div>
-
-                        <div class="form-text">
+                    <?php echo do_shortcode('[wpforms id="109" title="false"]'); ?>
+                    <div class="form-text">
                             No credit card required. Cancel anytime.
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
