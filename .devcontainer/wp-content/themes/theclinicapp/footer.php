5221ea4e426d6003e39255803965f701d8c23c73
Tito Tawfik <tawfik.bakr@gmail.com>
2025-09-26 14:08:41 +0000

    footer widgets changes

--- .devcontainer/wp-content/themes/theclinicapp/footer.php
+++ .devcontainer/wp-content/themes/theclinicapp/footer.php
@@ -7,48 +7,72 @@
  */
 ?>
 
-<footer class="footer-container bg-grey-600 py-5 border-top-grey" id="footer">
+<footer class="footer-container bg-grey-600 pt-5 pb-1 border-top-grey" id="footer">
     <div class="container">
         <div class="row">
-            <div class="col-lg-5 col-md-8 mb-4">
-                <h5 class="text-grey-100 fw-bold">About  <?php bloginfo('name'); ?></h5>
-                <p class="text-grey-100 pe-2 text-justify"><?php echo  get_bloginfo('description'); ?></p>
+            <div class="col-lg-4 col-md-6 mb-4 text-white widget-footer widget-about">
+                <?php if(is_active_sidebar('footer-1')): ?>
+                    <?php dynamic_sidebar('footer-1'); ?>
+                <?php else: ?>
+                    <h6 class="text-white fw-bold">About <?php bloginfo('name'); ?></h6>
+                    <p class="text-white pe-2 text-justify"><?php echo get_bloginfo('description'); ?></p>
+                <?php endif; ?>
             </div>
-            <div class="col-lg-2 col-md-4 mb-4">
-                <!-- Footer 1 the company: -->
-                <div class="widget-footer widget-html widget-company">
-                <?php
-                    if ( is_active_sidebar( 'footer-1' ) ) {
-                        dynamic_sidebar( 'footer-1' );
-                    }
-                ?>
-                </div>        
-            </div>
-            <div class="col-lg-3 col-md-6 mb-4">
-                <!-- Footer 2 the services pages list: -->
-                <div class="widget-footer widget-navlist widget-navlist-services">
+
+            <div class="col-lg-3 col-md-6 mb-4 text-white widget-footer widget-navlist widget-navlist-services">
                 <?php
                     if ( is_active_sidebar( 'footer-2' ) ) {
                         dynamic_sidebar( 'footer-2' );
-                    }
+                    } else {
+                    echo '<h6 class="text-white fw-bold">Software For</h6>';
+                    wp_nav_menu(array(
+                    'theme_location' => 'services-menu',
+                    'container'      => false,
+                    'menu_class'     => 'footer-links',
+                ));
+                }
                 ?>
-                </div>        
             </div>
-             <div class="col-lg-2 col-md-6 mb-4">
-                <h6 class="text-white fw-bold">Resources</h6>
+            <div class="col-lg-3 col-md-6 mb-4 text-white widget-footer widget-navlist widget-navlist-features">
                 <?php
-                    
-                   wp_nav_menu(array(
-                        'theme_location' => 'footer-menu',  
-                        'container'      => false,           
-                        'menu_class'     => 'footer-links list-unstyled'       
-                    )); 
+                    if ( is_active_sidebar( 'footer-3' ) ) {
+                        dynamic_sidebar( 'footer-3' );
+                    } else {
+                    echo '<h6 class="text-white fw-bold">Software Features</h6>';
+                    wp_nav_menu(array(
+                    'theme_location' => 'features-menu',
+                    'container'      => false,
+                    'menu_class'     => 'footer-links',
+                ));
+                }
                 ?>
             </div>
-            <div class="footer-bottom-container d-flex justify-content-between py-5">
-                <div class="col-md-8 mb-4">
+            <div class="col-lg-2 col-md-6 mb-4 widget-footer widget-html widget-company">
+                <?php
+                    if ( is_active_sidebar( 'footer-4' ) ) {
+                        dynamic_sidebar( 'footer-4' );
+                    } 
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom-container border-top-grey pt-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-4">
                     <p class="mb-0 text-grey-100">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
                 </div>
+                <div class="col-md-4 mb-4">
+                <?php
+
+                wp_nav_menu(array(
+                    'theme_location' => 'footer-menu',
+                    'container'      => false,
+                    'menu_class'     => 'footer-links d-flex justify-content-evenly',
+                ));
+                ?>
+            </div>
                 <div class="col-md-4 mb-4">
                     <ul class="footer-social-links list-unstyled d-flex text-grey-100 float-md-end">
                         <li><a href="https://www.youtube.com/@TheClinicApp" target="_blank" title="Visit our youtube" class="text-grey-100 me-3"><i class="fab fa-youtube fa-xl" aria-hidden="true"></i></a></li>
@@ -57,21 +81,25 @@
                 </div>
             </div>
         </div>
+    </div>
 
 </footer>
 
 <?php wp_footer(); ?>
+
 <script>
-  const iframe = document.getElementById('signuptrail');
-  window.addEventListener('message', (event) => {
-      // Optional: Verify the origin
-  if (event.origin !== "https://trial.theclinicapp.com") return;
-  if (event.data.type === 'setHeight') {
-    const scrollY = window.scrollY; // Save scroll position
-    iframe.style.height = event.data.height + 'px';
-    window.scrollTo({ top: scrollY }); // Restore it
-  }
-});
+    const iframe = document.getElementById('signuptrail');
+    window.addEventListener('message', (event) => {
+        // Optional: Verify the origin
+        if (event.origin !== "https://trial.theclinicapp.com") return;
+        if (event.data.type === 'setHeight') {
+            const scrollY = window.scrollY; // Save scroll position
+            iframe.style.height = event.data.height + 'px';
+            window.scrollTo({
+                top: scrollY
+            }); // Restore it
+        }
+    });
 </script>
 </body>
 
--- .devcontainer/wp-content/themes/theclinicapp/functions.php
+++ .devcontainer/wp-content/themes/theclinicapp/functions.php
@@ -36,35 +36,55 @@ function register_site_menus()
 	register_nav_menus(
 		array(
 			'header-menu' => __('Header Menu'),
-			'footer-menu' => __('Footer Menu')
+			'footer-menu' => __('Footer Menu'), 
+			'services-menu' => __('Services Menu'),
+			'features-menu' => __('Features Menu')
 		)
 	);
 }
 add_action('init', 'register_site_menus');
 /** ============================================================================================================================================= */
 // Register Footer Widgets
-function mytheme_footer_widgets_init() {
+function theclinicapp_footer_widgets_init() {
     register_sidebar( array(
         'name'          => __( 'Footer 1', 'theclinicapp' ),
         'id'            => 'footer-1',
         'description'   => __( 'Footer widget area 1', 'theclinicapp' ),
-        'before_widget' => '<div id="%1$s" class="widget text-white %2$s">',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
         'after_widget'  => '</div>',
-        'before_title'  => '<h6 class="widget-title text-white fw-bold">',
+        'before_title'  => '<h6 class="widget-title fw-bold">',
         'after_title'   => '</h6>',
     ) );
-
     register_sidebar( array(
         'name'          => __( 'Footer 2', 'theclinicapp' ),
         'id'            => 'footer-2',
         'description'   => __( 'Footer widget area 2', 'theclinicapp' ),
-        'before_widget' => '<div id="%1$s" class="widget text-white %2$s">',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
         'after_widget'  => '</div>',
-        'before_title'  => '<h6 class="widget-title text-white fw-bold">',
+        'before_title'  => '<h6 class="widget-title fw-bold">',
         'after_title'   => '</h6>',
     ) );
+	register_sidebar( array(
+        'name'          => __( 'Footer 3', 'theclinicapp' ),
+        'id'            => 'footer-3',
+        'description'   => __( 'Footer widget area 3', 'theclinicapp' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h6 class="widget-title fw-bold">',
+        'after_title'   => '</h6>',
+    ) );
+	register_sidebar( array(
+	'name'          => __( 'Footer 4', 'theclinicapp' ),
+	'id'            => 'footer-4',
+	'description'   => __( 'Footer widget area 4', 'theclinicapp' ),
+	'before_widget' => '<div id="%1$s" class="widget text-white %2$s">',
+	'after_widget'  => '</div>',
+	'before_title'  => '<h6 class="widget-title text-white fw-bold">',
+	'after_title'   => '</h6>',
+) );
 }
-add_action( 'widgets_init', 'mytheme_footer_widgets_init' );
+add_action( 'widgets_init', 'theclinicapp_footer_widgets_init' );
+
 /** ============================================================================================================================================= */
 // DISABLE USER REST API CALL
 function disable_rest_endpoints($endpoints)
